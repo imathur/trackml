cba0e92c78c0a82c556cbe679a92d9aa8a758820
Ishita Mathur <ishita.mathur@live.com>
2018-06-09 12:28:46 +0530

    Initial commit having project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Code for Kaggle's TrackML challenge',
+    author='Ishita Mathur',
+    license='MIT',
+)
